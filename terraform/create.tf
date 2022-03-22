08bbb8f55c71dc1cdc33dd8a6c35a95ef8c531c3
nekrasova <nekrasova@tecforce.ru>
2022-03-22 22:11:51 +0300

    1

--- terraform/create.tf
+++ terraform/create.tf
@@ -14,8 +14,8 @@ provider "aws" {
 resource "aws_instance" "builder" {
   ami = "ami-092cce4a19b438926"
   instance_type = "t3.micro"
-  key_name = "my-key001"
-  vpc_security_group_ids = ["sg-014aa96269c6d3b0d"]
+  key_name = "my-key01"
+  vpc_security_group_ids = ["sg-0bc242d0a1be0d8f2"]
   tags = {
     Name = var.name1
     group = "builder"
@@ -25,8 +25,8 @@ resource "aws_instance" "builder" {
 resource "aws_instance" "stage" {
   ami = "ami-092cce4a19b438926"
   instance_type = "t3.micro"
-  key_name = "my-key001"
-  vpc_security_group_ids = ["sg-014aa96269c6d3b0d"]
+  key_name = "my-key01"
+  vpc_security_group_ids = ["sg-0bc242d0a1be0d8f2"]
   tags = {
     Name = var.name2
     group = "stage"
