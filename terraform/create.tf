0d02b6db0f9fd6351379023bc4d2e3fe504788ab
kupa2004 <98742842+kupa2004@users.noreply.github.com>
2022-03-22 14:53:24 +0300

    Update and rename congif.tf to terraform/create.tf

---
+++ terraform/create.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "builder" {
+  ami = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  key_name = "my-key-test"
+  vpc_security_group_ids = ["sg-0bc242d0a1be0d8f2"]
+  tags = {
+    Name = "buildvm"
+    group = "builder"
+  }
+}
+
+resource "aws_instance" "stage" {
+  ami = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  key_name = "my-key-test"
+  vpc_security_group_ids = ["sg-0bc242d0a1be0d8f2"]
+  tags = {
+    Name = "stagevm"
+    group = "stage"
+  }
+}
