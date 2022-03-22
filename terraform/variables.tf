73f38ff947616f25ca2fea664fbb1159559ea0aa
kupa2004 <98742842+kupa2004@users.noreply.github.com>
2022-03-22 15:56:42 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,6 @@
-variable "name" {
-    description= "The name of the instance we are creating"
+variable "name1" {
+    description= "The name of the instance1 we are creating"
+}
+variable "name2" {
+    description= "The name of the instance2 we are creating"
 }
